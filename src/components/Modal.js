bd967135bb5cb67ffa87e7f4d78f16567e2c1bbb
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-03-25 19:40:31 +0530

    Fixed positioned parent by using react portal.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,21 @@
+import ReactDOM from "react-dom";
 
-const Modal = ({setOpenModal}) => {
-  return (
+const Modal = ({ setOpenModal }) => {
+  return ReactDOM.createPortal(
     <div>
       <div className="modal-layover">
         <div className="modal-message">
           <div className="modal-message_button-wrapper">
-            <button className="btn" onClick={() => setOpenModal(false)}>X</button>
+            <button className="btn" onClick={() => setOpenModal(false)}>
+              X
+            </button>
           </div>
-          <p>
-            I am a modal.
-          </p>
+          <p>I am a modal.</p>
         </div>
       </div>
-    </div>
-  )
+    </div>,
+    document.querySelector(".modal-container")
+  );
 };
 
 export default Modal;
